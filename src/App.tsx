ab5e4bc8fb1c4d57256f59214f07485ea58bda60
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-02 13:31:35 -0400

    Use esbuild-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const startService = async () => {
+        const result = await esbuild.initialize({
+            wasmURL: "./node_modules/esbuild-wasm/esbuild.wasm",
+            worker: false,
+        });
+        return result;
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
