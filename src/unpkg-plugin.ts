a2aecf73030551e5bf00ff290c27f313a35d9992
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-07 14:02:12 -0500

    Start bundling relative import

--- src/unpkg-plugin.ts
+++ src/unpkg-plugin.ts
@@ -1,34 +1,39 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
-import { ba } from "./a";
+import { ba, base } from "./a";
 
 export const unpkgPathPlugin = () => {
     return {
         name: "unpkg-path-plugin",
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                return { path: args.path, namespace: "a" };
+                return { path: args.path, namespace: "http-url" };
             });
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: `
-              import message from './message';
+            build.onResolve(
+                { filter: /^\.\//, namespace: "http-url" },
+                async (args: any) => {
+                    return { path: new URL(args.path, base).toString() };
+                }
+            );
+
+            build.onLoad(
+                { filter: /.*/, namespace: "http-url" },
+                async (args: any) => {
+                    if (args.path === "index.js") {
+                        return {
+                            loader: "jsx",
+                            contents: `
+              import message from 'medium-test';
               console.log(message);
             `,
-                    };
-                } else if (args.path === "./message") {
-                    const result = await axios.get(ba);
-                    return { loader: "js", contents: result.data };
-                } else {
-                    return {
-                        loader: "jsx",
-                        contents: 'export default message = "hi"',
-                    };
+                        };
+                    } else {
+                        const result = await axios.get(args.path);
+                        return { loader: "js", contents: result.data };
+                    }
                 }
-            });
+            );
         },
     };
 };
