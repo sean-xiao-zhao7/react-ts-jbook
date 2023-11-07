7a10100c08cdc068535647392ac29137b9a075b3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-07 13:07:32 -0500

    Use axios in unpkg plugin

--- src/unpkg-plugin.ts
+++ src/unpkg-plugin.ts
@@ -1,17 +1,16 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import { ba } from "./a";
 
 export const unpkgPathPlugin = () => {
     return {
         name: "unpkg-path-plugin",
         setup(build: esbuild.PluginBuild) {
             build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log("onResole", args);
                 return { path: args.path, namespace: "a" };
             });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log("onLoad", args);
-
                 if (args.path === "index.js") {
                     return {
                         loader: "jsx",
@@ -21,10 +20,8 @@ export const unpkgPathPlugin = () => {
             `,
                     };
                 } else {
-                    return {
-                        loader: "jsx",
-                        contents: 'export default "hi there!"',
-                    };
+                    const result = await axios.get(ba);
+                    return { loader: "js", contents: result.data };
                 }
             });
         },
