b919010b8a619f947a6d1147a892d8513579630c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-09 13:09:58 -0500

    Extract index.js to its own onLoad

--- src/plugins/fetch-plugins.ts
+++ src/plugins/fetch-plugins.ts
@@ -11,46 +11,48 @@ export const fetchPlugin = (userInput: string) => {
         name: "unpkg-path-plugin",
         setup(build: esbuild.PluginBuild) {
             build.onLoad(
+                { filter: /(^index\.js$)/, namespace: "http-url" },
+                async () => {
+                    return {
+                        loader: "jsx",
+                        contents: userInput,
+                    };
+                }
+            );
+            build.onLoad(
                 { filter: /.*/, namespace: "http-url" },
                 async (args: any) => {
-                    if (args.path === "index.js") {
-                        return {
-                            loader: "jsx",
-                            contents: userInput,
-                        };
+                    let data, responseURL;
+                    const cacheData = await unpkgStaticCache.getItem<{
+                        data: any;
+                        responseURL: string;
+                    }>(args.path);
+                    if (cacheData) {
+                        data = cacheData.data;
+                        responseURL = cacheData.responseURL;
                     } else {
-                        let data, responseURL;
-                        const cacheData = await unpkgStaticCache.getItem<{
-                            data: any;
-                            responseURL: string;
-                        }>(args.path);
-                        if (cacheData) {
-                            data = cacheData.data;
-                            responseURL = cacheData.responseURL;
-                        } else {
-                            const result = await axios.get(args.path);
-                            data = result.data;
-                            responseURL = result.request.responseURL;
+                        const result = await axios.get(args.path);
+                        data = result.data;
+                        responseURL = result.request.responseURL;
 
-                            await unpkgStaticCache.setItem(args.path, {
-                                data,
-                                responseURL,
-                            });
-                        }
-                        return {
-                            loader: "js",
-                            contents: data,
-                            pluginData:
-                                args.pluginData && args.pluginData.baseImport
-                                    ? {
-                                          baseUrl: responseURL
-                                              .split("/")
-                                              .slice(0, -1)
-                                              .join("/"),
-                                      }
-                                    : {},
-                        };
+                        await unpkgStaticCache.setItem(args.path, {
+                            data,
+                            responseURL,
+                        });
                     }
+                    return {
+                        loader: "js",
+                        contents: data,
+                        pluginData:
+                            args.pluginData && args.pluginData.baseImport
+                                ? {
+                                      baseUrl: responseURL
+                                          .split("/")
+                                          .slice(0, -1)
+                                          .join("/"),
+                                  }
+                                : {},
+                    };
                 }
             );
         },
