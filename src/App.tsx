55f9b37e6591d19bce6684b2adac7fde2a249345
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-14 13:15:08 -0500

    Use SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // CSS
 import "../node_modules/bulmaswatch/superhero/bulmaswatch.min.css";
-import "./App.css";
+import "./App.scss";
 
 // components
 import CodePane from "./components/code-pane/CodePane";
