907debf85e2889080956b1c664b47a6ecbce4555
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-14 11:35:48 -0500

    Re-add transform into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+// esbuild
+import * as esbuild from "esbuild-wasm";
+// esbuild plugins
+import { unpkgPathPlugin } from "./plugins/unpkg-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugins";
+
+// monaco
 import Editor from "@monaco-editor/react";
 
 // CSS
@@ -11,6 +19,72 @@ import Preview from "./components/Preview";
 const App = () => {
     const [sourceCode, setSourceCode] = useState("");
     const [loader, setLoader] = useState("js");
+    const [loading, setLoading] = useState(false);
+
+    const transform = async () => {
+        setLoading(true);
+        let result1 = await esbuild.build({
+            entryPoints: [`index.${loader}`],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin(), fetchPlugin(sourceCode)],
+            outfile: "out.js",
+        });
+
+        // if (result1.outputFiles.length > 1) {
+        //     let combinedText = "";
+        //     for (const outputFile of result1.outputFiles) {
+        //         combinedText += "----\n";
+        //         combinedText += outputFile.text;
+        //     }
+        //     setTransformedCode(combinedText);
+        // } else {
+        //     setTransformedCode(result1.outputFiles[0].text);
+        // }
+        iframeRef.current.srcdoc = execCode;
+        iframeRef.current.contentWindow.postMessage(
+            result1.outputFiles[0].text,
+            "*"
+        );
+
+        setLoading(false);
+    };
+
+    const iframeRef = useRef<any>();
+
+    const initESBW = useCallback(async () => {
+        try {
+            await esbuild.initialize({
+                wasmURL: "/esbuild.wasm",
+            });
+        } catch (err: any) {
+            console.log(err.message);
+        }
+    }, []);
+
+    useEffect(() => {
+        initESBW();
+    }, [initESBW]);
+
+    const execCode = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data)
+                    } catch (err) {                        
+                        const root = document.querySelector('#root');                        
+                        root.innerHTML = '<p style="color: red;">' + err + '.</p>';
+                        console.error(err);
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>
+    `;
 
     return (
         <div className="cols-container">
@@ -53,7 +127,11 @@ const App = () => {
                     Transform
                 </button>
             </div>
-            <Preview loader={loader} sourceCode={sourceCode} />
+            <Preview
+                loading={loading}
+                iframeRef={iframeRef}
+                execCode={execCode}
+            />
         </div>
     );
 };
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,83 +1,10 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-
-// esbuild
-import * as esbuild from "esbuild-wasm";
-// esbuild plugins
-import { unpkgPathPlugin } from "../plugins/unpkg-plugin";
-import { fetchPlugin } from "../plugins/fetch-plugins";
-
 interface PreviewProps {
-    sourceCode: string;
-    loader: string;
+    loading: boolean;
+    iframeRef: any;
+    execCode: string;
 }
 
-const Preview = ({ sourceCode, loader }: PreviewProps) => {
-    const [loading, setLoading] = useState(false);
-    const iframeRef = useRef<any>();
-
-    const initESBW = useCallback(async () => {
-        try {
-            await esbuild.initialize({
-                wasmURL: "/esbuild.wasm",
-            });
-        } catch (err: any) {
-            console.log(err.message);
-        }
-    }, []);
-
-    const transform = async () => {
-        setLoading(true);
-        let result1 = await esbuild.build({
-            entryPoints: [`index.${loader}`],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(sourceCode)],
-            outfile: "out.js",
-        });
-
-        // if (result1.outputFiles.length > 1) {
-        //     let combinedText = "";
-        //     for (const outputFile of result1.outputFiles) {
-        //         combinedText += "----\n";
-        //         combinedText += outputFile.text;
-        //     }
-        //     setTransformedCode(combinedText);
-        // } else {
-        //     setTransformedCode(result1.outputFiles[0].text);
-        // }
-        iframeRef.current.srcdoc = execCode;
-        iframeRef.current.contentWindow.postMessage(
-            result1.outputFiles[0].text,
-            "*"
-        );
-
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        initESBW();
-    }, [initESBW]);
-
-    const execCode = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try {
-                        eval(event.data)
-                    } catch (err) {                        
-                        const root = document.querySelector('#root');                        
-                        root.innerHTML = '<p style="color: red;">' + err + '.</p>';
-                        console.error(err);
-                    }
-                }, false);
-            </script>
-        </body>
-    </html>
-    `;
-
+const Preview = ({ loading, iframeRef, execCode }: PreviewProps) => {
     return (
         <div className="col" style={{ backgroundColor: "white" }}>
             {loading && <h4>Transforming</h4>}
